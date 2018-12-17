7a23d7560927cc2acb88744e8b0b577f85ea77ab
Lina Shejwal <lshejwal@snappydata.io>
2018-12-17 16:41:05 +0530

    else statement to change locator2 to 1 removed

--- init.sh
+++ init.sh
@@ -163,31 +163,29 @@ if [ "$LOCATORNODECOUNT" == "2" ]; then
   echo ${LOCATORHOSTNAME} | grep '1$'
   if [ $? == 0 ]; then
     OTHER_LOCATOR=`echo ${LOCATORHOSTNAME} | sed 's/1$/2/g'`
-  else
-    OTHER_LOCATOR=`echo ${LOCATORHOSTNAME} | sed 's/2$/1/g'`
   fi
 fi
-
 if [ "$NODETYPE" == "locator" ]; then
-    if [ ${OTHER_LOCATOR} != "" ]; then
-      OTHER_LOCATOR="-locators=${OTHER_LOCATOR}:10334"
-    fi
-    echo "${LOCAL_IP} -peer-discovery-address=${LOCAL_IP} ${OTHER_LOCATOR} -hostname-for-clients=${PUBLIC_IP} -dir=/opt/snappydata/work/locator ${CONFPARAMETERS}" > ${DIR}/conf/locators 
-    ${DIR}/sbin/snappy-locators.sh start
+  if [ ${OTHER_LOCATOR} != "" ]; then
+    OTHER_LOCATOR="-locators=${OTHER_LOCATOR}:10334"
+  fi
+  echo "${LOCAL_IP} -peer-discovery-address=${LOCAL_IP} ${OTHER_LOCATOR} -hostname-for-clients=${PUBLIC_IP} -dir=/opt/snappydata/work/locator ${CONFPARAMETERS}" > ${DIR}/conf/locators 
+  ${DIR}/sbin/snappy-locators.sh start
 fi
 
 if [ ${OTHER_LOCATOR} != "" ]; then
-    OTHER_LOCATOR=",${OTHER_LOCATOR}:10334"
+  OTHER_LOCATOR=",${OTHER_LOCATOR}:10334"
 fi
 
 if [ "$NODETYPE" == "datastore" ]; then
-    echo "${LOCAL_IP} -locators=${LOCATORHOSTNAME}:10334${OTHER_LOCATOR} -hostname-for-clients=${PUBLIC_IP} -dir=/opt/snappydata/work/datastore ${CONFPARAMETERS}" > ${DIR}/conf/servers
-    ${DIR}/sbin/snappy-servers.sh start
+  echo "${LOCAL_IP} -locators=${LOCATORHOSTNAME}:10334${OTHER_LOCATOR} -hostname-for-clients=${PUBLIC_IP} -dir=/opt/snappydata/work/datastore ${CONFPARAMETERS}" > ${DIR}/conf/servers
+  ${DIR}/sbin/snappy-servers.sh start
 fi
 
-if [ "$NODETYPE" == "lead" ]; then
-    echo "${LOCAL_IP} -locators=${LOCATORHOSTNAME}:10334${OTHER_LOCATOR} -dir=/opt/snappydata/work/lead ${CONFPARAMETERS}" > ${DIR}/conf/leads
-    ${DIR}/sbin/snappy-leads.sh start
+elif [ "$NODETYPE" == "lead" ]; then
+  echo "${LOCAL_IP} -locators=${LOCATORHOSTNAME}:10334${OTHER_LOCATOR} -dir=/opt/snappydata/work/lead ${CONFPARAMETERS}" > ${DIR}/conf/leads
+  ${DIR}/sbin/snappy-leads.sh start
 fi
+
 # ---------------------------------------------------------------------------------------------
 
