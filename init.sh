ab451b3959e50855b22e258845cab5551cf97834
Kajal Hulage <kajal.hulage15@vit.edu>
2018-12-20 18:34:13 +0530

    zeppeline interpreter.setting.json is updated

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# init.sh -t NODETYPE -i LOCALIP -s STARTADDRESS -c DATASTORENODECOUNT -a LOCATOR1HOSTNAME -b LOCATOR2HOSTNAME -u BASEURL
-# sh init.sh -d standard -t locator -i 10.0.1.4 -s 10.0.1.4 -c 2 -a av-locator1 -b av-locator2 -u https://raw.githubusercontent.com/arsenvlad/snappydata-azure/master
+# init.sh -t NODETYPE -c DATASTORENODECOUNT -l LOCATORHOSTNAME -u BASEURL -a ADMINUSER -n LOCATORNODECOUNT -z LAUNCHZEPPELIN -f CONFPARAMETERS
+# sh init.sh -t locator -c 3 -l sd-locator1 -u https://raw.githubusercontent.com/hulagekajal/snappydata-azure/master -a azureuser -n 1 -z yes -f -heap-size=4g
 
 log()
 {
     echo "$1"
-    logger "$1"
+    logger "[SNAPPYDATA] $1"
 }
 
 # Initialize local variables
@@ -14,14 +14,10 @@ log()
 NOW=$(date +"%Y%m%d")
 
 # Get command line parameters
-while getopts "t:i:s:c:l:u:a:n:z:f:" opt; do
+while getopts "t:c:l:u:a:n:z:f:" opt; do
     log "Option $opt set with value (${OPTARG})"
     case "$opt" in
         t) NODETYPE=$OPTARG
-        ;;
-        i) LOCALIP=$OPTARG
-        ;;
-        s) STARTADDRESS=$OPTARG
         ;;
         c) DATASTORENODECOUNT=$OPTARG
         ;;
@@ -76,14 +72,6 @@ if [[ -z ${NODETYPE} ]]; then
     fatal "No node type -t specified, can't proceed."
 fi
 
-if [[ -z ${LOCALIP} ]]; then
-    fatal "No local IP -i specified, can't proceed."
-fi
-
-if [[ -z ${STARTADDRESS} ]]; then
-    fatal "No start address -s specified, can't proceed."
-fi
-
 if [[ -z ${DATASTORENODECOUNT} ]]; then
     fatal "No datastore count -c specified, can't proceed."
 fi
@@ -105,28 +93,10 @@ if [[ -z ${LOCATORNODECOUNT} ]]; then
 fi
 
 
-log "init.sh NOW=$NOW NODETYPE=$NODETYPE LOCALIP=$LOCALIP STARTADDRESS=$STARTADDRESS DATASTORENODECOUNT=$DATASTORENODECOUNT BASEURL=$BASEURL LOCATORNODECOUNT=$LOCATORNODECOUNT"
-
-# Just a helper method example in case it is convenient to get all IPs into a file by doing some math on the starting IP and the count of data store nodes
-create_internal_ip_file()
-{
-    # Generate IP addresses of the nodes based on the convention of locator1, leader1, data stores
-    IFS='.' read -r -a startaddress_parts <<< "$STARTADDRESS"
-    for (( c=0; c<4+$DATASTORENODECOUNT; c++ ))
-    do
-        octet1=${startaddress_parts[0]}
-        octet2=${startaddress_parts[1]}
-        octet3=$(( ${startaddress_parts[2]} + $(( $((${startaddress_parts[3]} + c)) / 256 )) ))
-        octet4=$(( $(( ${startaddress_parts[3]} + c )) % 256 ))
-        ip=$octet1"."$octet2"."$octet3"."$octet4
-        echo $ip
-    done > ${INTERNAL_IP_FILE}
-}
+log "init.sh NOW=$NOW NODETYPE=$NODETYPE DATASTORENODECOUNT=$DATASTORENODECOUNT BASEURL=$BASEURL LOCATORNODECOUNT=$LOCATORNODECOUNT"
 
 launch_zeppelin()
 {
-    
-
     ZEP_URL_MIRROR="http://archive.apache.org/dist/zeppelin/zeppelin-0.7.3/zeppelin-0.7.3-bin-netinst.tgz"
     ZEP_NOTEBOOKS_URL="https://github.com/SnappyDataInc/zeppelin-interpreter/raw/notes/examples/notebook"
     ZEP_NOTEBOOKS_DIR="notebook"
@@ -136,51 +106,58 @@ launch_zeppelin()
     chown -R ${ADMINUSER}:${ADMINUSER} /opt/zeppelin
    
    
- # download zeppelin 0.7.3 distribution, extract as /opt/zeppelin
-    echo "Downloading Zeppelin distribution from ${ZEP_URL_MIRROR} ..."
+    # download zeppelin 0.7.3 distribution, extract as /opt/zeppelin
+    log "Downloading Zeppelin distribution from ${ZEP_URL_MIRROR} ..."
     wget -q "${ZEP_URL_MIRROR}"
     tar -xf "zeppelin-0.7.3-bin-netinst.tgz" --directory ${Z_DIR} --strip 1
     
     # download pre-created sample notebooks for snappydata
-    echo "Downloading Zeppelin notebooks from ${ZEP_NOTEBOOKS_URL}/${ZEP_NOTEBOOKS_DIR}.tar.gz ..."
+    log "Downloading Zeppelin notebooks from ${ZEP_NOTEBOOKS_URL}/${ZEP_NOTEBOOKS_DIR}.tar.gz ..."
     wget -q "${ZEP_NOTEBOOKS_URL}/${ZEP_NOTEBOOKS_DIR}.tar.gz"
     tar -xzf "${ZEP_NOTEBOOKS_DIR}.tar.gz"
     find ${ZEP_NOTEBOOKS_DIR} -type f -print0 | xargs -0 sed -i "s/localhost/${PUBLIC_HOSTNAME}/g"
 
-    echo "Copying sample notebooks..."
+    log "Copying sample notebooks ..."
     cp -ar "${ZEP_NOTEBOOKS_DIR}/." "${Z_DIR}/${ZEP_NOTEBOOKS_DIR}/"
 
     # download zeppelin interpreter 0.7.3.4 for snappydata
     ZEP_INTP_JAR="snappydata-zeppelin_2.11-0.7.3.4.jar"
     INTERPRETER_URL="https://github.com/SnappyDataInc/zeppelin-interpreter/releases/download/v0.7.3.4/${ZEP_INTP_JAR}"
-    INTERPRETER_DIR="${Z_DIR}/interpreter/snappydata"
-    mkdir -p "${INTERPRETER_DIR}"
     wget -q "${INTERPRETER_URL}"
     mv "${ZEP_INTP_JAR}" "${DIR}/"
-    # mv "${ZEP_INTP_JAR}" "${INTERPRETER_DIR}"
 
     ${Z_DIR}/bin/install-interpreter.sh --name snappydata --artifact io.snappydata:snappydata-zeppelin:0.7.3.4
 
-#    jar -xf "${INTERPRETER_DIR}/${ZEP_INTP_JAR}" interpreter-setting.json
-#    mv interpreter-setting.json interpreter-setting.json.orig
-
-    # Place interpreter dependencies into the directory
-    # cp -a "${DIR}/jars/." "${INTERPRETER_DIR}"
-#    cp interpreter-setting.json.orig "${INTERPRETER_DIR}"/interpreter-setting.json
-  
     # Modify conf/zeppelin-site.xml to include classnames of snappydata interpreters.
-#    if [[ ! -e "${Z_DIR}/conf/zeppelin-site.xml" ]]; then
     cp "${Z_DIR}/conf/zeppelin-site.xml.template" "${Z_DIR}/conf/zeppelin-site.xml"
     SEARCH_STRING="<name>zeppelin.interpreters<\/name>"
     INSERT_STRING="org.apache.zeppelin.interpreter.SnappyDataZeppelinInterpreter,org.apache.zeppelin.interpreter.SnappyDataSqlZeppelinInterpreter,"
     sed -i "/${SEARCH_STRING}/{n;s/<value>/<value>${INSERT_STRING}/}" "${Z_DIR}/conf/zeppelin-site.xml"
-#    fi
-    # edit conf/zeppelin-site.xml (add our two interpreter classnames under 'interpreters' attribute.
 
     # optional: generate interpreter.json by restarting the zeppelin server and point zeppelin to remote interpreter process at localhost:3768
-    # start zeppelin server
-    "${Z_DIR}/bin/zeppelin-daemon.sh start"
+    log "Configuring Snappydata Interpreter..."
+    ${Z_DIR}/bin/zeppelin-daemon.sh start
+    while ! test -f  "${Z_DIR}/conf/interpreter.json" ; do
+      sleep 3
+    done
+    sh "${Z_DIR}/bin/zeppelin-daemon.sh" stop
+    if [[ ! -e "${Z_DIR}/conf/interpreter.json" ]]; then
+      log "The file interpreter.json was not generated."
+    fi
+
+    # Modify conf/interpreter.json to include lead host and port and set isExistingProcess to true.
+    if [[ -e "${Z_DIR}/conf/interpreter.json" ]]; then
+      LEAD_HOST="localhost"
+      LEAD_PORT="3768"
+      sed -i "/group\": \"snappydata\"/,/isExistingProcess\": false/{s/isExistingProcess\": false/isExistingProcess\": snappydatainc_marker/}" "${Z_DIR}/conf/interpreter.json"
+      sed -i "/snappydatainc_marker/a \"host\": \"${LEAD_HOST}\",\n \"port\": \"${LEAD_PORT}\"," "${Z_DIR}/conf/interpreter.json"
+      sed -i "s/snappydatainc_marker/true/" "${Z_DIR}/conf/interpreter.json"
+    fi
+
+    # Start zeppelin server
+    ${Z_DIR}/bin/zeppelin-daemon.sh start
 }
+
 # ============================================================================================================
 # MAIN
 # ============================================================================================================
@@ -191,20 +168,13 @@ export DIR=/opt/snappydata
 mkdir -p ${DIR}
 
 # TODO Get the latest snappydata distribution
-wget --tries 10 --retry-connrefused --waitretry 15 https://github.com/SnappyDataInc/snappydata/releases/download/v1.0.2.1/snappydata-1.0.2.1-bin.tar.gz
+wget -q --tries 10 --retry-connrefused --waitretry 15 https://github.com/SnappyDataInc/snappydata/releases/download/v1.0.2.1/snappydata-1.0.2.1-bin.tar.gz
 
 # Extract the contents of the archive to /opt/snappydata directory without the top folder
 tar -zxf snappydata-1.0.2.1-bin.tar.gz --directory ${DIR} --strip 1
 
 cd ${DIR}
 
-# Uncomment if you want to download test tools and data sets
-# wget --tries 10 --retry-connrefused --waitretry 15 https://sdtests.blob.core.windows.net/testdata/scripts.tgz
-# wget --tries 10 --retry-connrefused --waitretry 15 https://sdtests.blob.core.windows.net/testdata/snappy-cluster_2.10-0.5-tests.jar
-# wget --tries 10 --retry-connrefused --waitretry 15 https://sdtests.blob.core.windows.net/testdata/TPCH-1GB.zip
-# wget --tries 10 --retry-connrefused --waitretry 15 https://sdtests.blob.core.windows.net/testdata/zeppelin.tgz
-
-# The start of services in proper order takes place based on dependsOn within the template: locators, data stores, leaders
 LOCAL_IP=`hostname -I`
 PUBLIC_IP=`curl ifconfig.co`
 
@@ -239,13 +209,13 @@ if [ "$NODETYPE" == "datastore" ]; then
   echo "${LOCAL_IP} -hostname-for-clients=${PUBLIC_IP} -dir=/opt/snappydata/work/datastore -locators=${LOCATORHOSTNAME}:10334${OTHER_LOCATOR} ${CONFPARAMETERS}" > ${DIR}/conf/servers
   ${DIR}/sbin/snappy-servers.sh start
 elif [ "$NODETYPE" == "lead" ]; then
-   if [ "$LAUNCHZEPPELIN" == "yes" ]; then
-      echo "${LOCAL_IP} -dir=/opt/snappydata/work/lead -locators=${LOCATORHOSTNAME}:10334${OTHER_LOCATOR} -zeppelin-interpreter-enable=true -classpath=${DIR}/snappydata-zeppelin_2.11-0.7.3.4.jar ${CONFPARAMETERS}" > ${DIR}/conf/leads
+    if [ "$LAUNCHZEPPELIN" == "yes" ]; then
+      echo "${LOCAL_IP} -dir=/opt/snappydata/work/lead -locators=${LOCATORHOSTNAME}:10334${OTHER_LOCATOR} -zeppelin.interpreter.enable=true -classpath=${DIR}/snappydata-zeppelin_2.11-0.7.3.4.jar ${CONFPARAMETERS}" > ${DIR}/conf/leads
       launch_zeppelin
-   else
+    else
       echo "${LOCAL_IP} -dir=/opt/snappydata/work/lead -locators=${LOCATORHOSTNAME}:10334${OTHER_LOCATOR} ${CONFPARAMETERS}" > ${DIR}/conf/leads
-   fi
-  ${DIR}/sbin/snappy-leads.sh start
+    fi
+    ${DIR}/sbin/snappy-leads.sh start
 fi
 
 # ---------------------------------------------------------------------------------------------
